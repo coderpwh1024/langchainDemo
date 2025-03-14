b757aacce12d89109acb6e8cbfc25f1310d83562
pengwenhao <pengwenhao@aliyun.com>
2025-03-14 17:50:02 +0800

    fix:update StructuredMultiple

--- langchainDemo/StructuredMultiple.py
+++ langchainDemo/StructuredMultiple.py
@@ -1,30 +1,50 @@
-from typing import Union, Optional
+from typing import Union, Optional, TypedDict
 
 from pydantic import BaseModel, Field
 from langchain_openai import AzureChatOpenAI
+from typing_extensions import Annotated, TypedDict
 
 
-class Joke(BaseModel):
+# Pydantic
+# class Joke(BaseModel):
+#     """Joke to tell user."""
+#     setup: str = Field(description="The setup of the joke")
+#     punchline: str = Field(description="The punchline of the joke")
+#     rating: Optional[int] = Field(
+#         default=None, description="How funny the joke is,from 1 to 10"
+#     )
+
+# Pydantic
+# class ConversationalResponse(BaseModel):
+#     """Respond in a conversational manner. Be kind and helpful."""
+#     response: str = Field(description="A conversational response to the user's query")
+
+# Pydantic
+# class FinalResponse(BaseModel):
+#     final_output: Union[Joke, ConversationalResponse]
+
+
+# TypedDict
+class Joke(TypedDict):
     """Joke to tell user."""
-    setup: str = Field(description="The setup of the joke")
-    punchline: str = Field(description="The punchline of the joke")
-    rating: Optional[int] = Field(
-        default=None, description="How funny the joke is,from 1 to 10"
-    )
+    setup: Annotated[str, ..., "The setup of the joke"]
+    setup: Annotated[str, ..., "The punchline of the joke"]
+    rating: Annotated[Optional[int], None, "How funny the joke is,from 1 to 10"]
 
 
-class ConversationalResponse(BaseModel):
+# TypedDict
+class ConversationalResponse(TypedDict):
     """Respond in a conversational manner. Be kind and helpful."""
-    response: str = Field(description="A conversational response to the user's query")
+    response: Annotated[str, ..., "A conversational response to the user's query"]
 
 
-class FinalResponse(BaseModel):
+class FinalResponse(TypedDict):
     final_output: Union[Joke, ConversationalResponse]
 
 
-endpoint = ""
-deployment = ""
-apiKey = ""
+endpoint = " "
+deployment = " "
+apiKey = " "
 
 model = AzureChatOpenAI(
     azure_endpoint=endpoint,
