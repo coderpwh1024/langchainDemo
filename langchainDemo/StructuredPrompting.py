b054845af687e7f73a04c2d52abfdc778d21b5a7
pengwenhao <pengwenhao@aliyun.com>
2025-03-14 18:18:09 +0800

    feat:add StructuredPrompting

---
+++ langchainDemo/StructuredPrompting.py
@@ -0,0 +1,24 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchainDemo.Prompt import prompt
+from langchainDemo.StructuredOutput import structured_llm
+
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
+
+example_user: Tell me another joke about planes
+example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
+
+example_user: Now about caterpillars
+example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+few_shot_structured_llm =  prompt|structured_llm
+few_shot_structured_llm.invoke("what's something funny about woodpeckers")
+
