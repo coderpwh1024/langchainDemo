80d5fb19f2c2aae80bae29e2931a99c5d877c807
pengwenhao <pengwenhao@aliyun.com>
2025-06-03 17:37:31 +0800

    fix:update agent

--- graph/Agent.py
+++ graph/Agent.py
@@ -1,5 +1,6 @@
 import os
 from langchain.chat_models import init_chat_model
+from pygments.lexer import combined
 from tenacity import retry
 
 apiKey = ""
@@ -89,13 +90,15 @@ workflow.add_edge("improve_joke", "polish_joke")
 workflow.add_edge("polish_joke", END)
 
 chain = workflow.compile()
+# display(Image(chain.get_graph().draw_mermaid_png()))
+# Show workflow
 display(Image(chain.get_graph().draw_mermaid_png()))
 
-state = chain.invoke("topic", "cats")
+state = chain.invoke({"topic": "cats"})
 print("Initial joke")
 print(state["joke"])
 print("\n--- --- ---\n")
-if  "improved_joke" in state:
+if "improved_joke" in state:
     print("Improvted joke:")
     print(state["improved_joke"])
     print("\n--- --- ---\n")
@@ -106,4 +109,63 @@ if  "improved_joke" in state:
 else:
     print("Joke failed quality gate - no punchline detected!")
 
+print(
+    "--------------------------------------------------------------------------------------------------------------------------------")
 
+
+class State(TypedDict):
+    topic: str
+    joke: str
+    story: str
+    poem: str
+    combined_output: str
+
+
+def call_llm_1(state: State):
+    """First LLM call to generate initial joke"""
+    msg = llm.invoke(f"Write a joke about {state['topic']}")
+    return {"joke": msg.content}
+
+
+def call_llm_2(state: State):
+    """Second LLM call to generate story"""
+    msg = llm.invoke(f"Write a story about {state['topic']}")
+    return {"story": msg.content}
+
+
+def call_llm_3(state: State):
+    """Third LLM call to generate poem"""
+    msg = llm.invoke(f"Write a poem about {state['topic']}")
+    return {"poem": msg.content}
+
+
+def aggregator(state: State):
+    """Combine the joke and story into a single output"""
+    combined = f"Here's a story,joke,and poem about {state['topic']}!\n\n"
+    combined += f"STORY:\n{state['story']}\n\n"
+    combined += f"JOKE:\n{state['joke']}\n\n"
+    combined += f"POEM:\n{state['poem']}\n\n"
+    return {"combined_output": combined}
+
+
+# 编译
+parallel_builder = StateGraph(State)
+
+parallel_builder.add_node("call_llm_1", call_llm_1)
+parallel_builder.add_node("call_llm_2", call_llm_2)
+parallel_builder.add_node("call_llm_3", call_llm_3)
+parallel_builder.add_node("aggregator", aggregator)
+
+parallel_builder.add_edge(START, "call_llm_1")
+parallel_builder.add_edge(START, "call_llm_2")
+parallel_builder.add_edge(START, "call_llm_3")
+parallel_builder.add_edge("call_llm_1", "aggregator")
+parallel_builder.add_edge("call_llm_2", "aggregator")
+parallel_builder.add_edge("call_llm_3", "aggregator")
+
+parallel_workflow = parallel_builder.compile()
+
+display(Image(parallel_workflow.get_graph().draw_mermaid_png()))
+
+state =  parallel_workflow.invoke({"topic": "cats"})
+print(state["combined_output"])
