b0b408fee7a72243280b4b643fec30427332df49
pengwenhao <pengwenhao@aliyun.com>
2025-06-04 14:25:51 +0800

    fix:update Agent

--- graph/Agent.py
+++ graph/Agent.py
@@ -167,5 +167,93 @@ parallel_workflow = parallel_builder.compile()
 
 display(Image(parallel_workflow.get_graph().draw_mermaid_png()))
 
-state =  parallel_workflow.invoke({"topic": "cats"})
+state = parallel_workflow.invoke({"topic": "cats"})
 print(state["combined_output"])
+
+
+print(
+    "--------------------------------------------------------------------------------------------------------------------------------")
+
+print(
+    "------------------------------------------------------------路由--------------------------------------------------------------------")
+
+
+# 路由功能
+from typing_extensions import Literal
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+class Route(BaseModel):
+    step: Literal["poem", "story", "joke"] = Field(None, description="The next step in the routing proces")
+
+
+router = llm.with_structured_output(Route)
+
+
+class State(TypedDict):
+    input: str
+    decision: str
+    output: str
+
+
+def llm_call_1(state: State):
+    """Write a story"""
+    result = llm.invoke(state["input"])
+    return {"output": result.content}
+
+
+def llm_call_2(state: State):
+    """Write a joke"""
+    result = llm.invoke(state["input"])
+    return {"output": result.content}
+
+
+def llm_call_3(state: State):
+    """Write a poem"""
+    result = llm.invoke(state["input"])
+    return {"output": result.content}
+
+
+def llm_call_router(state: State):
+    """Route the input to the appropriate node """
+    decision = router.invoke(
+        [
+            SystemMessage(content="Route the input to story, joke, or poem based on the user's request."),
+            HumanMessage(content=state["input"])
+        ]
+    )
+    return {"decision": decision.content}
+
+
+def route_decision(state: State):
+    if state["decision"] == "story":
+        return "llm_call_1"
+    elif state["decision"] == "joke":
+        return "llm_call_2"
+    elif state["decision"] == "poem":
+        return "llm_call_3"
+
+
+router_builder = StateGraph(State)
+
+router_builder.add_node("llm_call_1", llm_call_1)
+router_builder.add_node("llm_call_2", llm_call_2)
+router_builder.add_node("llm_call_3", llm_call_3)
+router_builder.add_node("llm_call_router", llm_call_router)
+
+router_builder.add_edge(START, "llm_call_router")
+router_builder.add_conditional_edges("llm_call_router", route_decision, {
+    "llm_call_1": "llm_call_1",
+    "llm_call_2": "llm_call_2",
+    "llm_call_3": "llm_call_3"
+},)
+
+router_builder.add_edge("llm_call_1", END)
+router_builder.add_edge("llm_call_2", END)
+router_builder.add_edge("llm_call_3", END)
+
+router_workflow = router_builder.compile()
+display(Image(router_workflow.get_graph().draw_mermaid_png()))
+
+state = router_workflow.invoke({"input": "Write me a joke about cats"})
+print(state["output"])
