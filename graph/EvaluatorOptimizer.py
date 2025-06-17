f2992dc6436e7ec37ead2d24b09b982e96a3ac16
pengwenhao <pengwenhao@aliyun.com>
2025-06-17 22:21:50 +0800

    feat:update EvaluatorOptimizer

---
+++ graph/EvaluatorOptimizer.py
@@ -0,0 +1,49 @@
+from dataclasses import Field
+from typing import TypedDict, Literal
+
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel
+
+apiKey = ""
+endpoint = ""
+open_ai_version = "2024-08-01-preview"
+azure_deployment = ""
+
+llm = init_chat_model(
+    "azure_openai:gpt-4.0",
+    azure_deployment=azure_deployment,
+    azure_endpoint=endpoint,
+    api_key=apiKey,
+    openai_api_version=open_ai_version,
+)
+
+
+class State(TypedDict):
+    joke: str
+    topic: str
+    feedback: str
+    funny_or_not: str
+
+
+class Feedback(BaseModel):
+    grade: Literal["funny", "not funny"] = Field(
+        description="Decide if the joke is funny or not.",
+    )
+    feedback: str = Field(
+        description="If the joke is not funny, provide feedback on how to improve it.",
+    )
+
+
+evaluator = llm.with_structured_output(Feedback)
+
+
+def llm_call_generator(state: State) -> Feedback:
+    """LLM generates a joke"""
+
+    if state.get("feedback"):
+        msg = llm.invoke(
+            f"Write a joke about {state['topic']} but take into account the feedback: {state['feedback']}"
+        )
+    else:
+        msg = llm.invoke(f"Write a joke about {state['topic']}")
+    return {"joke": msg.content}
